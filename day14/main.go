776b6ee85e8b5f87defb83e47c30c810cc338275
James Menzies <james.r.menzies@gmail.com>
2024-12-01 14:26:19 +1100

    add other day dirs

---
+++ day14/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/james-menzies/aoc-template-go/internal"
+)
+
+func main() {
+	input := internal.ReadInput("day14/input.txt")
+	fmt.Printf("First Solution: %v, Second Solution: %v", solution1(input), solution2(input))
+}
+
+func solution1(input []string) any {
+	// TODO solve first challenge
+	return nil
+}
+
+func solution2(input []string) any {
+	// TODO solve second challenge
+	return nil
+}
